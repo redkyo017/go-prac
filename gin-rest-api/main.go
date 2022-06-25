dabcacf4e6ad2bb7be61fcab175a4e10d2388fe4
hung.han <hung.han@setel.com>
2022-06-25 16:26:53 +0700

    update

--- gin-rest-api/main.go
+++ gin-rest-api/main.go
@@ -8,6 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books = []Book{
+	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
+	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
+	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+}
+
 func main() {
 	fmt.Println("con co be be")
 	log.Println("con meo bay bay")
@@ -16,6 +28,22 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello World!",
 		})
+	})
+	r.GET("/books", func(c *gin.Context) {
+		c.JSON(http.StatusOK, books)
+	})
+
+	r.POST("/books", func(c *gin.Context) {
+		var book Book
+		if err := c.ShouldBindJSON(&book); err != nil {
+			log.Println("hehee", err)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		books = append(books, book)
+		c.JSON(http.StatusCreated, books)
 	})
 	r.Run()
 }
