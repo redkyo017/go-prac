1a82dcc4f5f40e707dcec8046da20fc9c89e3590
hung.han <hung.han@setel.com>
2022-08-15 09:24:48 +0700

    go-webapp-gin-1

--- go-webapp-gin-1/main.go
+++ go-webapp-gin-1/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "log"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
 
 func main() {
-	log.Println("con co be be")
+	router = gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+
+	})
+	router.Run()
 }
